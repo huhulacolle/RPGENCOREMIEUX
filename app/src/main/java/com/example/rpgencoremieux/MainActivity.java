f28bde38f62f5fd9ec745bf651c04a811bed5b0b
Hugo Araujo <araujohugo@msn.com>
2020-12-02 11:16:45 +0100

    Menu incroyable

--- app/src/main/java/com/example/rpgencoremieux/MainActivity.java
+++ app/src/main/java/com/example/rpgencoremieux/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.rpgencoremieux;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button NewGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.NewGame = (Button) findViewById(R.id.NewGame);
+        NewGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent debut = new Intent(getApplicationContext(), Commencement.class);
+                startActivity(debut);
+                finish();
+
+            }
+        });
     }
 }
