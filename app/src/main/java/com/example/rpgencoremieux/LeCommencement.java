ebc8e39575122647e0c16a26e0f89bd65360a052
Gianni <GianniB77@users.noreply.github.com>
2021-03-10 11:53:58 +0100

    Slatum2

--- app/src/main/java/com/example/rpgencoremieux/LeCommencement.java
+++ app/src/main/java/com/example/rpgencoremieux/LeCommencement.java
@@ -2,13 +2,27 @@ package com.example.rpgencoremieux;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LeCommencement extends AppCompatActivity {
 
+    private Button Carte;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_le_commencement);
+        this.Carte = findViewById(R.id.next);
+
+        Carte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent launchNG = new Intent(getApplicationContext(), CarteUltime.class);
+                startActivity(launchNG);
+                finish();
+            }
+        });
     }
 }
