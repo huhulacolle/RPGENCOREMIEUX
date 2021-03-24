3e70bc82f4ca97e434ba02468f7fa0ebbf6479ce
Gianni <GianniB77@users.noreply.github.com>
2021-03-24 11:50:26 +0100

    lentement mais surement

--- app/src/main/java/com/example/rpgencoremieux/Laforet2.java
+++ app/src/main/java/com/example/rpgencoremieux/Laforet2.java
@@ -2,13 +2,27 @@ package com.example.rpgencoremieux;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Laforet2 extends AppCompatActivity {
 
+    private Button Gauche;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laforet2);
+        this.Gauche = findViewById(R.id.next6);
+        getSupportActionBar().hide();
+        Gauche.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent launchNG = new Intent(getApplicationContext(), Laforet2.class);
+                startActivity(launchNG);
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/rpgencoremieux/SortieForet.java
@@ -0,0 +1,14 @@
+package com.example.rpgencoremieux;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SortieForet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sortie_foret);
+    }
+}
